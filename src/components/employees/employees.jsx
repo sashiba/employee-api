e19cbd7840e71ceb42d9e6f2a000c8a1ed76ddc8
Alexander Pirnarev <sashopirnarev@gmail.com>
2020-03-28 23:26:06 +0200

    add update message

--- src/components/employees/employees.jsx
+++ src/components/employees/employees.jsx
@@ -4,30 +4,69 @@ import './employees.css';
 class Employees extends Component {
   state = {
     employees: [],
+    sorting: 'age',
+    display: 'none',
   };
 
   async componentDidMount() {
     await this.refreshData();
     this.interval = setInterval(() => this.refreshData(), 10000);
+    this.intervalSortChange = setInterval(() => this.changeSort(), 10000);
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    const { employees } = this.state;
+
+    if (prevState.employees.length > 0) {
+      if (prevState.employees[0].id !== employees[0].id) {
+        this.setState({ updated: true });
+
+        this.notif.classList.add('display-flex');
+      }
+    }
   }
 
   componentWillUnmount() {
     clearInterval(this.interval);
+    clearInterval(this.intervalSortChange);
   }
 
   handleClick = (event) => {
     event.currentTarget.classList.toggle('active');
-    event.currentTarget.children[0].classList.toggle('employee-expanded-display');
+    event.currentTarget.children[0].classList.toggle('display-flex');
+  };
+
+  sortByAge = (employees) => {
+    return employees.sort((a, b) => a.employee_age - b.employee_age);
+  };
+
+  sortBySalary = (employees) => {
+    return employees.sort((a, b) => a.employee_salary - b.employee_salary);
+  };
+
+  changeSort = () => {
+    const { sorting } = this.state;
+    const newSorting = sorting === 'age' ? 'salary' : 'age';
+    console.log('Sorting: ', newSorting);
+
+    this.setState({ sorting: newSorting });
   };
 
   async refreshData() {
     const dummyUrlAPI = 'http://dummy.restapiexample.com/api/v1/employees';
+    const { sorting } = this.state;
 
     try {
       const res = await fetch(dummyUrlAPI);
       const employees = await res.json();
+      let sortedData;
+      if (sorting === 'age') {
+        sortedData = this.sortByAge(employees.data);
+      } else {
+        sortedData = this.sortBySalary(employees.data);
+      }
       this.setState({
-        employees: employees.data.slice(0, 10),
+        employees: sortedData.slice(0, 10),
       });
     } catch (err) {
       console.log('error: ', err);
@@ -53,16 +92,21 @@ class Employees extends Component {
     const { employees } = this.state;
 
     return (
-      <div className="container-employees">
-        {employees.length &&
-          employees.map((employee) =>
-            this.renderEmployee(
-              employee.id,
-              employee.employee_name,
-              employee.employee_salary,
-              employee.employee_age
-            )
-          )}
+      <div>
+        <div className="notification" ref={(notif) => (this.notif = notif)}>
+          Updated employees!
+        </div>
+        <div className="container-employees">
+          {employees.length &&
+            employees.map((employee) =>
+              this.renderEmployee(
+                employee.id,
+                employee.employee_name,
+                employee.employee_salary,
+                employee.employee_age
+              )
+            )}
+        </div>
       </div>
     );
   }
